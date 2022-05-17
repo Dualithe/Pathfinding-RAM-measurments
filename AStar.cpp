f50c87d3957b9b937288e99f84ae17877c87e1dd
Dualithe <dualithe@gmail.com>
2022-05-17 16:24:16 +0200

    Pathfinding

--- AStar.cpp
+++ AStar.cpp
@@ -9,44 +9,53 @@ class AStar() {
 	int size;
 	int** board;
 
-	std::vector<Node> closedList;
-	std::vector<Node> openList;
-
+	std::vector<Node*> closedList;
+	std::vector<Node*> openList;
+	Node* endNode = NULL;
 
 public:
 
-	struct Vec2 {
+	struct Vec2
+	{
 		int x;
 		int y;
-		Vec2(int x, int y) {
+		Vec2(int x, int y)
+
+		{
 			this->x = x;
 			this->y = y;
 		}
-		Vec2() {
+		Vec2()
+		{
 			x = 0;
 			y = 0;
 		}
 	};
 
-	struct Node {
+	struct Node
+	{
 		Vec2 pos;
 		int g = 0;
 		int h = 0;
 		int f = 0; //g+h
+		Node* parent = NULL;
 	};
 
 
-	AStar(int** board, int size) {
+	AStar(int** board, int size)
+	{
 		this->size = size;
 		this->board = board;
 	}
 
 
-	bool isPosValid(Vec2 pos) {
+	bool isPosValid(Vec2 pos)
+	{
 		return pos.x >= 0 && pos.y >= 0 && pos.x < size&& pos.y < size;
 	}
 
-	std::vector<Vec2> getAdjacentPos(Vec2 vec) {
+	std::vector<Vec2> getAdjacentPos(Vec2 vec)
+	{
 
 		std::vector<vec> result;
 
@@ -66,57 +75,92 @@ public:
 		return result;
 	}
 
-	void TryAddToOpen(Vec2 pos, int g, int h) {
-		for (Node node : openList) {
-			if (node.pos.x == pos.x && node.pos.y == pos.y) {
+	void TryAddToOpen(Vec2 pos, int g, int h, Node * parent)
+	{
+		for (Node* node : openList)
+		{
+			if (node->pos.x == pos.x && node->pos.y == pos.y)
+			{
 				return;
 			}
 		}
-		for (Node node : closedList) {
-			if (node.pos.x == pos.x && node.pos.y == pos.y) {
+		for (Node* node : closedList)
+		{
+			if (node->pos.x == pos.x && node->pos.y == pos.y)
+			{
 				return;
 			}
 		}
 
-		Node newNode;
-		newNode.g = g;
-		newNode.h = h;
-		newNode.f = g + h;
-		newNode.pos = pos;
+		Node* newNode = new Node;
+		newNode->g = g;
+		newNode->h = h;
+		newNode->f = g + h;
+		newNode->pos = pos;
+		newNode->parent = parent;
 		openList.push_back(newNode);
 	}
 
-	void calculate(Vec2 startPos, Vec2 endPos) {
-
-		Node start;
-		start.pos = startPos;
-
-		tryAddToOpen(startPos, 0, h);
-
-		while (openList.size() > 0) {
-			Node currentNode = popBestNode();
-
-			std::vector<Vec2> poses = getAdjacentPos(start.pos);
-
-			for (Vec2 pos : poses) {  //foreach
-				int g = currentNode.g + 10;
-				int h = std::abs(pos.x - endPos.x) + std::abs(pos.y - endPos.y);
-				TryAddToOpen(pos);
-			}
-		}
-
-
-
-
+	std::vector<Node*> calculatePath(Vec2 startPos,Vec2 endPos) 
+	{
+		calculate();
+		getPath();
 	}
 
-	Node popBestNode() {
+	void calculate(Vec2 startPos, Vec2 endPos)
+	{
+
+		int h = std::abs(startPos.x - endPos.x) + std::abs(startPos.y - endPos.y);
+		tryAddToOpen(startPos, 0, h);
+
+		while (openList.size() > 0)
+		{
+			Node* currentNode = popBestNode();
+
+			if (currentNode->pos.x == endPos.x && currentNode->pos.y == endPos.y)
+			{
+				endNode = currentNode;
+				break;
+			}
+
+			std::vector<Vec2> poses = getAdjacentPos(currentNode->pos);
+
+			for (Vec2 pos : poses)  //foreach
+			{
+				int g = currentNode->g + 10;
+				int h = std::abs(pos.x - endPos.x) + std::abs(pos.y - endPos.y);
+				TryAddToOpen(pos, g, h, currentNode);
+			}
+		}
+	}
+
+	std::vector<Node*> getPath()
+	{
+		std::vector<Node*> path;
+
+		if (endNode != NULL)
+		{
+			Node* node = endNode;
+			do {
+				path.push_front(node);
+				node = node->parent;
+
+			} while (node != NULL);
+		}
+
+		return path;
+	}
+
+	Node* popBestNode()
+	{
 		int minF = 1000000;
 
 
-		for (Node node : openList) {
-			if (node.f < minF) {
-				minF = node.f;
+		for (Node* node : openList)
+		{
+			if (node->f < minF)
+			{
+				minF = node->f;
 			}
 		}
 
@@ -125,15 +169,17 @@ public:
 
 		Node bestNodeIdx;
 
-		for (int i = 0; i < openList.size(); i++) {
-			Node noder = openList[i];
-			if (node.f == minF && node.h < minH) {
-				minH = node.h;
+		for (int i = 0; i < openList.size(); i++)
+		{
+			Node* node = openList[i];
+			if (node->f == minF && node->h < minH)
+			{
+				minH = node->h;
 				bestNodeIdx = i;
 			}
 		}
 
-		Noder bestNode = openList[i];
+		Node* bestNode = openList[i];
 		openList.remove(openList.begin() + i);
 		closedList.push_back(bestNode);
 		return bestNode;
